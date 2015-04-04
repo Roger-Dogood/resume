2c993e6b67d8290c7d1aa0aa4ff7f3d4bab0913b
Matthew Dahl <daftdahl@gmail.com>
2015-04-03 17:22:07 -0700

    Fixed broken tasks

--- builds/development/js/template.js
+++ builds/development/js/template.js
@@ -1,20 +1,22 @@
 var $        = require("jquery"),
     Mustache = require('mustache');
 
-// Get Header Object from JSON
-$(function() {
-  $.getJSON('../js/data.json', function(data) {
-    var template = $('#headertpl').html();
-    var html = Mustache.to_html(template, data);
-    $('#header').html(html);
-  });
+// Get Header Object from data.json
+$(function () {
+    'use strict';
+    $.getJSON('../js/data.json', function (data) {
+        var template = $('#headertpl').html(),
+            html = Mustache.to_html(template, data);
+        $('#header').html(html);
+    });
 });
 
-// Get Resume Object from JSON
-$(function() {
-  $.getJSON('../js/data.json', function(data) {
-    var template = $('#resumetpl').html();
-    var html = Mustache.to_html(template, data);
-    $('#resume').html(html);
-  });
+// Get Resume Object from data.json
+$(function () {
+    'use strict';
+    $.getJSON('../js/data.json', function (data) {
+        var template = $('#resumetpl').html(),
+            html = Mustache.to_html(template, data);
+        $('#resume').html(html);
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
+/* jslint node: true */
+/*jslint todo: true */
+'use strict';
+
 var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
+    glob        = require('glob'),
     browserify  = require('browserify'),
-    transform   = require('vinyl-transform'),
-    compass     = require('gulp-compass'),
+    source      = require('vinyl-source-stream'),
+    buffer      = require('vinyl-buffer'),
+    sass        = require('gulp-ruby-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
     browserSync = require('browser-sync'),
     uglify      = require('gulp-uglify'),
     minifyHTML  = require('gulp-minify-html'),
@@ -16,69 +23,78 @@ var env,
 
 env = process.env.NODE_ENV || 'development';
 
-devDir = 'builds/development/';
-outputDir = 'builds/production/';
+devDir = './builds/development/';
+outputDir = './builds/production/';
 
 
-
-gulp.task('index', function() {
+gulp.task('index', function () {
     return gulp.src(devDir + '*.html')
         .pipe(minifyHTML())
         .pipe(gulp.dest(outputDir));
 });
 
-gulp.task('resume', function() {
+gulp.task('resume', function () {
     return gulp.src(devDir + 'resume/*.html')
         .pipe(minifyHTML())
         .pipe(gulp.dest(outputDir + 'resume'));
 });
 
-gulp.task('js', function() {
-    var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
+gulp.task('js', function () {
+    var entry = glob.sync(devDir + 'js/*.js'),
 
-    return gulp.src(devDir + 'js/*.js')
-        .pipe(browserified)
+        // set up the browserify instance on a task basis
+        b = browserify({
+            entries: entry,
+            extensions: ['.js'],
+            debug: true,
+            // defining transforms here will avoid crashing your stream
+            // transform: [browserify]
+        });
+
+    return b.bundle()
+        .pipe(source(devDir + 'js/template.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
         .pipe(concat('script.js'))
         .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(outputDir + 'js'));
 });
 
-gulp.task('json', function() {
+gulp.task('json', function () {
     return gulp.src(devDir + 'js/*.json')
         .pipe(jsonMinify())
         .pipe(gulp.dest(outputDir + 'js'));
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('compass', function() {
-    return gulp.src(devDir + 'scss/style.scss')
-        .pipe(compass({
-            sass: devDir + 'scss/',
-            image: outputDir + 'images',
-            style: 'compressed'
-        })
-            .on('error', gutil.log))
+gulp.task('sass', function () {
+    return sass(devDir + 'scss/style.scss', {
+        style: 'compressed',
+        sourcemap: true
+    })
+            .on('error', gutil.log)
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(outputDir + 'css'))
         .pipe(reload({stream: true}));
 });
 
-gulp.task('watch', function() {
+// TODO: Figure out what's wrong with my watch task.
+gulp.task('watch', function () {
     gulp.watch(devDir + 'js/*.js', ['js']).on('change', reload);
     gulp.watch(devDir + 'js/*.json', ['json']).on('change', reload);
-    gulp.watch(devDir + 'scss/**/*.scss', ['compass']);
+    gulp.watch(devDir + 'scss/*.scss', ['sass']);
     gulp.watch(devDir + 'resume/**/*.html', ['resume']).on('change', reload);
     gulp.watch(devDir + '*.html', ['index']).on('change', reload);
 });
 
 // Static Server
-gulp.task('serve', function() {
-
+gulp.task('serve', function () {
     browserSync({
         server: outputDir
     });
 });
 
-gulp.task('default', ['index', 'resume', 'js', 'json', 'compass', 'watch', 'serve']);
+gulp.task('default', ['index', 'resume', 'js', 'json', 'sass', 'watch', 'serve']);
